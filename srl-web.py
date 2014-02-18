df19583b488388440a5443a9eb221147bd4acfde
maxis <maxis1718@gmail.com>
2014-02-18 11:31:17 +0800

    modify srl web for testing

--- srl-web.py
+++ srl-web.py
@@ -4,12 +4,13 @@ import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
 
 print "Content-Type: text/html"
+print ""
 
 ###################################
-if __name__ == "__main__":
- import sys
- import socket
- try:
+if __name__ == '__main__':
+	import socket
+	import sys
+
 	print """
 
 	<html>
@@ -26,30 +27,26 @@ if __name__ == "__main__":
       <div><input type="submit" value="label"></div>
 	</form>
 	"""
-	form = cgi.FieldStorage() 
+
+	form = cgi.FieldStorage()
 	HOST, PORT = "localhost", 9999
-	
 	data = form.getvalue('tree')
 
 	# Create a socket (SOCK_STREAM means a TCP socket)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 	# Connect to server and send data
 	sock.connect((HOST, PORT))
 	sock.sendall(data + "\n")
 
 	# Receive data from the server and shut down
 	received = sock.recv(1024)
-	sock.close()	
+	sock.close()
+
 	print """<html><b>Unannotated:</b><br></html>"""
 	print data
 	print """<html><b><br><br><br>Annotated:<br></b></html>"""
 	print received
-	print """
-	</body>
- 
-	</html>
-	""" 
- 	
- except:
-	print >>sys.stderr, __doc__
-	raise
+	print """ </body> </html>"""
+	
+	
